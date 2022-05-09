3ce6d0fe270430b5d066286daef578e3e2582ef0
Carlos Alves [Kuro Suzume] <carlos.ca.eduardo@gmail.com>
2022-05-09 08:18:28 -0300

    adding new view AddStates to States
    Adding new view and adding functionality to State models.

--- Assignment1CarlosAlves/Assignment1CarlosAlves/Controllers/StatesController.cs
+++ Assignment1CarlosAlves/Assignment1CarlosAlves/Controllers/StatesController.cs
@@ -9,53 +9,120 @@ namespace Assignment1CarlosAlves.Controllers
 {
     public class StatesController : Controller
     {
+
+
+
         // GET: States
-        public ActionResult AllStates(int sortBy = 0, bool isDesc = false)
+        /// <summary>
+        /// The view returns a list of all states
+        /// </summary>
+        /// <param name="id">The search term</param>
+        /// <param name="sortBy">Integer. 0 = StateCode, 1 = stateName, 2 = first ZipCode, 3 = LastZip</param>
+        /// <returns></returns>
+        public ActionResult AllStates(string id, int sortBy = 0)
         {
+
             TechSupportEntities context = new TechSupportEntities();
+            //List<State> states = context.States.OrderBy(s => s.StateName).ToList();
             List<State> states;
 
             switch (sortBy)
             {
                 case 1:
                     {
-                        if (isDesc)
-                            states = context.States.OrderByDescending(s => s.StateCode).ToList();
-                        else
-                            states = context.States.OrderBy(s => s.StateCode).ToList();
+                        //if (isDesc)
+                        //    states = context.States.OrderByDescending(s => s.StateCode).ToList();
+                        //else
+                        states = context.States.OrderBy(s => s.StateName).ToList();
                         break;
                     }
                 case 2:
                     {
-                        if (isDesc)
-                            states = context.States.OrderByDescending(s => s.StateName).ToList();
-                        else
-                            states = context.States.OrderBy(s => s.StateName).ToList();
+                        //if (isDesc)
+                        //    states = context.States.OrderByDescending(s => s.StateName).ToList();
+                        //else
+                        states = context.States.OrderBy(s => s.FirstZipCode).ToList();
                         break;
                     }
 
                 case 3:
                     {
-                        if (isDesc)
-                            states = context.States.OrderByDescending(s => s.FirstZipCode).ToList();
-                        else
-                            states = context.States.OrderBy(s => s.FirstZipCode).ToList();
+                        //if (isDesc)
+                        //    states = context.States.OrderByDescending(s => s.FirstZipCode).ToList();
+                        //else
+                        states = context.States.OrderBy(s => s.LastZipCode).ToList();
                         break;
                     }
 
                 case 0:
                 default:
                     {
-                        if (isDesc)
-                            states = context.States.OrderByDescending(s => s.LastZipCode).ToList();
-                        else
-                            states = context.States.OrderBy(s => s.LastZipCode).ToList();
+                        //if (isDesc)
+                        //    states = context.States.OrderByDescending(s => s.LastZipCode).ToList();
+                        //else
+                        states = context.States.OrderBy(s => s.StateCode).ToList();
                         break;
                     }
+
+            }
+            //id is used as "searchTerm" parameter
+            if (!string.IsNullOrWhiteSpace(id))
+            {
+                id = id.Trim().ToLower();
+                int zipCodeLookup = 0;
+
+
+                //in case id is an integer
+                if (int.TryParse(id, out zipCodeLookup))
+                {
+                    Console.WriteLine(id);
+                    states = states.Where(s =>
+                               s.FirstZipCode == zipCodeLookup ||
+                               s.LastZipCode == zipCodeLookup
+                                ).ToList();
+                }
+                else // if id does not parse
+                {
+                    states = states.Where(s =>
+                      s.StateCode.ToLower().Contains(id) ||
+                      s.StateName.ToLower().Contains(id)
+                      ).ToList();
+                }
+
 
             }
 
             return View(states);
         }
+
+        [HttpGet]
+        public ActionResult AddStates()
+        {
+            State state = new State();
+            return View();
+        }
+
+        public ActionResult AddStates(State state)
+        {
+            TechSupportEntities context = new TechSupportEntities();
+
+            try
+            {
+                context.States.Add(state);
+
+                context.SaveChanges();
+
+            }
+            catch
+            {
+                //log the exception in the error log and send an automatic email to IT support
+                //throw;
+            }
+            return RedirectToAction("AllStates");
+        }
+
+
     }
+
+
 }
